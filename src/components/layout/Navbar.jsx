95ce0834ac529e93aea90652c002deb036f372ce
Mayur Kapadi <125800989+mkkapadi12@users.noreply.github.com>
2025-07-07 17:29:03 +0530

    change button import format

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 // import { usePathname } from "next/navigation"
 import { Menu, Sparkles } from "lucide-react";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/Button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Link, useNavigate } from "react-router-dom";
 
